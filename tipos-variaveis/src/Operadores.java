aa441ac150488dd310033b0afe51f8eeb762a4ee
Edgard Moraes <edgardmoraes@icloud.com>
2023-03-26 02:06:04 -0300

    acrescentado classe de operadores

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,14 @@
+public class Operadores {
+    public static void main (String [] args) {
+        String nomeCompleto = "LINGUAGEM" + "JAVA";
+        System.out.println(nomeCompleto);
+
+        int numero = 5;
+
+        System.out.println(- numero);
+        System.out.println(numero);
+    }
+
+
+    
+}
