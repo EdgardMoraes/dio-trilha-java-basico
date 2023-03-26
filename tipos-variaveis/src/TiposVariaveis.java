3075ccf7057874d7f2cc7e8c0cd562ecacc901bc
Edgard Moraes <edgardmoraes@icloud.com>
2023-03-26 01:52:39 -0300

    inserção de variaveis

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,25 @@
 public class TiposVariaveis {
+    /**
+     * @param args
+     * @throws Exception
+     */
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        double salarioMinimo = 2500;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        int numero = 5;
+        
+
+        System.out.println(numero);
+
+       final double VALOR_DE_PI = 3.14;
+
+       
+
+      
+
     }
 }
